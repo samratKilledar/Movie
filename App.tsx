2aba20a53c9b2535d456899e900ec20ccb82929f
samratkilledar <samratkilledar@gmail.com>
2025-05-27 19:57:37 +0530

    add safearea

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, LogBox } from 'react-native';
 import NetInfo, { NetInfoState } from '@react-native-community/netinfo';
 import StartApp from './src/StartApp';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const App: React.FC = () => {
   const [isConnected, setIsConnected] = useState<boolean>(true);
@@ -17,14 +18,16 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <View style={{ flex: 1 }}>
-      {!isConnected && (
-        <View style={styles.networkStrip}>
-          <Text style={styles.networkText}>Please turn on your data connection.</Text>
-        </View>
-      )}
-      <StartApp />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }} edges={['top', 'bottom', 'left', 'right']}>
+        {!isConnected && (
+          <View style={styles.networkStrip}>
+            <Text style={styles.networkText}>Please turn on your data connection.</Text>
+          </View>
+        )}
+        <StartApp />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,31 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, {useEffect, useState} from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
 import AuthNavigator from './AuthNavigator';
-import { getItem } from '../api/storageServices';
-import { ActivityIndicator, View } from 'react-native';
-import { useSelector } from 'react-redux';
+import {ActivityIndicator, View} from 'react-native';
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
   const [loading, setLoading] = useState(true);
-  useEffect(()=>{
-    setTimeout(()=>{setLoading(false)},2000)
-  })
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  });
 
   if (loading) {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <ActivityIndicator size="large" color="#0000ff" />
       </View>
     );
   }
 
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      
-        <Stack.Screen name="Auth" component={AuthNavigator} />
-  
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Auth" component={AuthNavigator} />
     </Stack.Navigator>
   );
 };
